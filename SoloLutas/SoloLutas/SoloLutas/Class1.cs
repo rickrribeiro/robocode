5d469fca2399d59bda03444bcc71b0e995ed752d
rickrribeiro <rickribeiro99@gmail.com>
2020-10-21 10:17:24 -0300

    robo feito v1

--- SoloLutas/SoloLutas/SoloLutas/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SoloLutas
-{
-    public class Class1
-    {
-    }
-}
---
+++ SoloLutas/SoloLutas/SoloLutas/SoloLutas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Robocode;
+
+
+namespace sololutas //SOL = sun   ULT= R   IS= é
+{
+    class SoloLutas : Robot
+    {
+      
+        public override void Run()
+        {
+            TurnLeft(Heading - 90);
+            TurnGunRight(90);
+            while (true)
+            {
+                Ahead(5000);
+                TurnRight(90);
+            }
+        }
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(Rules.MAX_BULLET_POWER); // lembrar de ver todos os maximos que apareceram (velocidade...)
+        }
+
+
+    }
+}
