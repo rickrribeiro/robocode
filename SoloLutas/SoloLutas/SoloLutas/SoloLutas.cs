ae6b88bb61d2e0820b45966aa947f9fef33a257a
rickrribeiro <rickribeiro99@gmail.com>
2020-10-21 13:47:57 -0300

    v3 9 a 1 corner 0a 10 tracker and alien

--- SoloLutas/SoloLutas/SoloLutas/SoloLutas.cs
+++ SoloLutas/SoloLutas/SoloLutas/SoloLutas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using Robocode;
@@ -9,29 +10,59 @@ namespace sololutas //SOL = sun   ULT= R   IS= é
 {
     class SoloLutas : AdvancedRobot
     {
-      
+        bool isShooting = false;
         public override void Run()
         {
-            
+            BodyColor = Color.Red;
+            GunColor = Color.Blue;
+            RadarColor = Color.White;
+            //   BulletColor = Color.FromArgb(70,77,105);
+            BulletColor = Color.Red;
+            MaxVelocity = Rules.MAX_VELOCITY;
+
+
+            int gunTurn = 3;
+            int robotTurn = 3;
+
             while (true)
             {
-           //     Fire(Rules.MAX_BULLET_POWER);
-           for(int i = 0; i < 2; i++)
-                {
-                    Ahead(150);
-                    TurnGunRight(30);
+                
+                Ahead(150);
+                for (int i = 0; i < 60; i++){
+                    TurnGunLeft(gunTurn);
                 }
-               
-                 TurnRight(35);
-
-
+                TurnRight(35);
+                TurnGunLeft(35);
             }
         }
+       
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
-            Fire(Rules.MAX_BULLET_POWER); // lembrar de ver todos os maximos que apareceram (velocidade...)
+       
+
+                Stop();     
+                Fire(Rules.MAX_BULLET_POWER); // lembrar de ver todos os maximos que apareceram (velocidade...)
+                Scan();
+                Resume();
+           
+            
         }
 
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            base.OnHitByBullet(evnt);
+            Ahead(100);
+            TurnRight(75);
+            TurnGunLeft(75);
+        }
+
+        public override void OnHitWall(HitWallEvent evnt)
+        {
+            base.OnHitWall(evnt);
+            TurnRight(75);
+        }
+
+     
 
     }
 }
